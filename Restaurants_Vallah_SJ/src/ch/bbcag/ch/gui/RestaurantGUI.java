4f291a3476c7ca93325f9cab91ad9d1535d3c4d5
zswobs <zswobs@WS-ZSWOBS.bbcnet-zh.local>
2018-02-20 07:48:26 +0100

    Fehler eingebaut

--- Restaurants_Vallah_SJ/src/ch/bbcag/ch/gui/RestaurantGUI.java
+++ Restaurants_Vallah_SJ/src/ch/bbcag/ch/gui/RestaurantGUI.java
@@ -9,7 +9,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
-
+.
 public class RestaurantGUI{
 	public static void main(String[] args) throws IOException{
 		// Das Fenster wo das GUI aufgebaut wird
